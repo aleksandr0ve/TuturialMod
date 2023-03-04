9e4dc5cd2a6d59a3f083f4b66a39d46b827e8f94
alexandr0ve <animeshnik-1999@yandex.ru>
2023-03-04 21:24:32 +0300

    Initial commit

---
+++ src/main/java/ru/aleksandr0ve/tutorialmod/TutorialMod.java
@@ -0,0 +1,10 @@
+package ru.aleksandr0ve.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TutorialMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/ru/aleksandr0ve/tutorialmod/client/TutorialModClient.java
@@ -0,0 +1,13 @@
+package ru.aleksandr0ve.tutorialmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
